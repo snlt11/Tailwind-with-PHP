986f5d4a6551592b70b6b0600cdc4d77ad78e3fb
Sai Nay Lin Thar <sainaylinthar@gmail.com>
2024-06-25 08:05:07 +0630

    Database & config

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+return[
+
+    'host' => 'localhost',
+    'dbname' => 'myapp',
+    'charset' => 'utf8mb4',
+    
+];
--- index.php
+++ index.php
@@ -3,3 +3,16 @@
 require_once("functions.php");
 
 require_once("router.php");
+
+require_once("Database.php");
+
+$config = require_once("config.php");
+
+$db = new Database($config);
+
+$posts = $db->query("SELECT * FROM posts")->fetchAll();
+
+
+foreach ($posts as $post) {
+    echo "<h1>" . $post['title'] . "</h1>";
+}
