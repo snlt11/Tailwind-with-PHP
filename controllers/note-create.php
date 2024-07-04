ed2199c838b0451857f488a30bad0bfc3056c313
Sai Nay Lin Thar <sainaylinthar@gmail.com>
2024-07-04 21:48:58 +0630

    feat: Validator

---
+++ controllers/note-create.php
@@ -0,0 +1,28 @@
+<?php
+
+$heading = "Create Note";
+
+require("Validator.php");
+
+$config = require_once("config.php");
+
+$db = new Database($config);
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $errors = [];
+    
+    if (! Validator::string($_POST['body'], 1, 1000)) {
+        $errors['body'] = "A body must be between 1 and 1000 characters long.";
+    }
+
+    if (empty($errors)) {
+        $db->query('INSERT INTO `notes`(`body`, `user_id`) VALUES (:body, :user_id)', [
+            'body' => $_POST['body'],
+            'user_id' => 1
+        ]);
+    }
+}
+
+require_once('./views/note-create.view.php');
--- views/notes.view.php
+++ views/notes.view.php
@@ -10,11 +10,14 @@
       <?php foreach ($notes as $note) : ?>
         <li>
           <a href="/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline">
-            <?= $note['body'] ?>
+            <?= htmlspecialchars($note['body']) ?>
           </a>
         </li>
       <?php endforeach; ?>
     </ul>
+    <p class="mt-6">
+      <a href="/notes/create" class="text-blue-500 hover:underline">Create Note</a>
+    </p>
   </div>
 
 </main>
